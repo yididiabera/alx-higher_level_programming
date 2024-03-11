3c6d3238350cbed975eba73bed976ce35de241b7
Jedidiah Abera <yididi@Jedidiah>
2024-03-11 20:30:54 +0300

    Done creating: 0x01-python-if_else_loops_functions

---
+++ 0x00-python-hello_world/0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def remove_char_at(str, n):
+    """
+    creates a copy of the string, removing the character at the position n
+    """
+    if n < 0:
+        return str
+    return (str[:n] + str[n+1:])
---
+++ 0x00-python-hello_world/0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    """a string in uppercase followed by a new line"""
+    for char in str:
+        if ord(char) >= 97 and ord(char) < 123:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
