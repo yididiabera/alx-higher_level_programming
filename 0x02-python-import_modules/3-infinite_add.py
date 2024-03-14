89e70c850329dc352bd27a7a7140e3d775088014
Jedidiah Abera <yididi@Jedidiah>
2024-03-14 21:19:36 +0300

    done

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """result of the addition of all arguments"""
+    import sys
+
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result+=int(sys.argv[i])
+    print(result)
+
