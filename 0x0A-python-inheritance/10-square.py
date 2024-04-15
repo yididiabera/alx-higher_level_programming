4538c3b6049bcd3719c77d5fc77b69d9cdae2dd6
Jedidiah Abera <yididi@Jedidiah>
2024-04-16 02:46:06 +0300

    Done creating: 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A Square class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Square class"""
+
+    def __init__(self, size):
+        """Initialize the rectangle class."""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Calculate area of rectangle"""
+        return self.__size * self.__size
