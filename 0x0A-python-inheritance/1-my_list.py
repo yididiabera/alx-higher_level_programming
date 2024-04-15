f1aac3451bb5a93c320f1e62504d1258bed83287
Jedidiah Abera <yididi@Jedidiah>
2024-04-16 02:37:30 +0300

    Done creating: 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class that inherits form class list."""
+
+class MyList(list):
+    """implements sorted printinig."""
+
+    def print_sorted(self):
+        """sortes the list."""
+        print(sorted(self))
