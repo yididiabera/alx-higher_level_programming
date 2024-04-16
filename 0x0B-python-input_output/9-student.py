c2eecfd6b00257dbb4a72d5153b9bcf9fb0ab633
Jedidiah Abera <yididi@Jedidiah>
2024-04-16 22:53:02 +0300

    Done creating: 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A module containing a student class
+"""
+
+
+class Student:
+    """A student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """A method that initializes a Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student"""
+        my_dict = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
+        return my_dict
