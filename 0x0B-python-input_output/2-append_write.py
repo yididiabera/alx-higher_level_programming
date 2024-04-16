007ebbfd6e077a0e527b7ed8eedd42befed6e7a6
Jedidiah Abera <yididi@Jedidiah>
2024-04-17 01:31:33 +0300

    done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""defines a function to appending to a file"""
+"""A module for appending to a file"""
 
 def append_write(filename="", text=""):
-    """Appends string to a file"""
+    """A function that appends to a file"""
     count = 0
     with open(filename, "a", encoding="utf-8") as f:
         count = f.write(text)
