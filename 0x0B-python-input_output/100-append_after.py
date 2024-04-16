3a86420716266ce25d88f43eb9ce48e3c234fe79
Jedidiah Abera <yididi@Jedidiah>
2024-04-17 01:04:33 +0300

    done

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-"""Define text appender function."""
+"""
+Module for append_after method.
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """inserts a line to a file, after each line containing a specific string."""
-    with open(filename, 'r+', encode='utf-8') as f:
-        for line in f.readlines():
-            if search_string in line:
-                f.write(new_string)
+    '''Method for inserting text after search string.'''
+    lines = []
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        i = 0
+        while i < len(lines):
+            if search_string in lines[i]:
+                lines[i:i + 1] = [lines[i], new_string]
+                i += 1
+            i += 1
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(lines)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""A module for adding cl args to a list and saves it"""
-import json
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    my_list = load("add_item.json")
-except Exception:
-    my_list = []
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-save(my_list, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
