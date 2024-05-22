a0b41285e68ac2293308ee2e6a6dd14abcba8c21
Jedidiah Abera <yididi@Jedidiah>
2024-05-22 23:34:12 +0300

    Done creating: 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Create user and grant all previleges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Create a MySQL server user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
