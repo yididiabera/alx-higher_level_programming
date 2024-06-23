328cfd9766d2f9fdf057a521faa444b357d9a6e6
Jedidiah Abera <yididi@Jedidiah>
2024-06-24 00:36:24 +0300

    Done creating: 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that takes in arguments and displays all
+values in the states table of hbtn_0e_0_usa where
+name matches the argument. But this time, write one
+that is safe from MySQL injections!
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+    searched_name = sys.argv[4]
+
+    # By default, it will connect to localhost:3306
+    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id",
+                (searched_name, ))
+    rows = cur.fetchall()
+
+    [print(row) for row in rows]
